1862432ad5ddd3c29c5398a83bee4fbee59036cc
TimeSentinel <lance.stubblefield@gmail.com>
2025-04-12 10:28:28 -0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+    return (
+        <>
+            <p>
+                Welcome to sol3. One planet with ALL it's people.
+
+            </p>
+            <p>
+
+            </p>
+        </>
+    )
+}
+
+export default App
