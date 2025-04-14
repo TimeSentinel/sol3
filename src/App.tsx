e619e40d6082294eb2d8d7ede92e7be01ca9bec7
TimeSentinel <lance.stubblefield@gmail.com>
2025-04-14 12:14:06 -0700

    My placeholder message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,68 @@
-
+import "./App.css"
 
 function App() {
 
     return (
         <>
+
             <p>
-                Welcome to sol3. One planet with ALL it's people.
+                Welcome to sol3.one <br/>
+                One planet with ALL it's people.
 
             </p>
             <p>
+                Site is in progress. <br/><br/>
+                My dream is for the people (ALL THE PEOPLE!) to unite and fix the world.<br/><br/>
+            </p>
+            <p>
+                The name...<br/>
+                SOL 3 is a space designation. <br/>The Sun is also known as Sol and we are the third planet in the Sol
+                System (Sol3).<br/>
+                One is for humanity. We are ONE group, not many. <br/>
+                It should not be us against them. It should be, 'how can I help?' or 'how can I make a difference?'.
+                <br/><br/>
+                <hr/>
+                The site will eventually be a huge forum to collect citizens of the planet. If everyone joins together,
+                governments and the wealthy will no longer have control.
+                <br/><br/>
+                It's very simple, but takes an enormous step. You (everyone) have to let go of racism, sexism, poverty
+                discrimination, and everything else that you see as a 'group' of people. <br/>
+                I know you are probably thinking that is obvious, but the part that will probably cause the most
+                backlash is asking the minorities to stop being minorities. <br/>We need to all take the step at
+                once and look at every person (EVERY PERSON!) on this planet as different. <br/><br/>
+                When it comes to rights and resources, everyone should be equal. <br/>When it comes to communication and
+                interactions, everyone is different. <br/><br/>
 
+            </p>
+            <p>
+                For now, if you have an interest in helping with this project, just send me an email. <a
+                href="mailto:lance@sol3.one">lance@sol3.one</a>.
+                <br/>
+
+            </p>
+            <p>
+                After a recent disappointment in my country's government (US), I can't help thinking the world needs to
+                change. Here in the US, we seem to be going backwards - degrading.<br/>
+                I thought of revolution... but the whole planet is broken and focusing on a single country or government
+                is not going to fix much. <br/> <br/>
+                How do we do it? <br/>First we need a majority of the planet's citizens talking. Everyone should have a
+                voice; the rich, the poor, any genetic make up,
+                people that support militaries, people who oppose them, people with disabilities, old people, young
+                people, any sex, any gender, any sexual preference, drug users, fitness gurus,
+                the anti-social and the mega-social, people who love animals, people who eat animals. Everyone gets a
+                voice. Everyone can participate in a conversation. <br/><br/>
+                The end goal is to eliminate people going hungry, eliminate wars amongst people, promote sharing,
+                promote helping, and working together.
+            </p>
+            <p>
+                Obviously, I need help. The foundation of this project must be as pure as possible. It can't be for
+                money. It can't be for promotion or pride. <br/>
+                It has to be built by those who truly care and have a vision of what the world NEEDS to look like.
+            </p>
+            <p>
+                I'm hoping this is a seed. I will create a forum, do some marketing on Google to get a few people to see it. <br/>
+                People need to want to join, to participate, to make a change. I am not raising money, because it should not be necessary. <br/>
+                We just need the people.
             </p>
         </>
     )
